6d3d7a376aa2dbc9102cbb17162fca4edae2cf3d
devarsh2007 <sunilpatel2131@gmail.com>
2024-07-16 09:00:33 +0530

    else if

---
+++ else_if4.c
@@ -0,0 +1,106 @@
+// write a program that perform below operation
+// 10 - one zero
+// 45 - four five
+
+#include<stdio.h>
+void main()
+{
+      int number,a,b;
+
+      printf("enter a number : ");
+      scanf("%d",&number);
+
+      a=number/10;
+      printf("%d-",a);
+
+      b=number%10;
+      printf("%d\n",b);
+
+      if(a==1)
+      {
+            printf("one");
+      }
+      else if(a==2)
+      {
+            printf("two");
+      }
+
+      else if(a==3)
+      {
+            printf("three");
+      }
+      else if(a==4)
+      {
+            printf("four");
+      }
+      else if(a==5)
+      {
+            printf("five");
+      }
+      else if(a==6)
+      {
+            printf("six");
+      }
+      else if(a==7)
+      {
+            printf("seven");
+      }
+      else if(a==8)
+      {
+            printf("eight");
+      }
+      else if(a==9)
+      {
+            printf("nine");
+      }
+      else if(a==0)
+      {
+            printf("zero");
+      }
+
+      printf(" ");
+
+      if(b==1)
+      {
+            printf("one");
+      }
+      else if(b==2)
+      {
+            printf("two");
+      }
+
+      else if(b==3)
+      {
+            printf("three");
+      }
+      else if(b==4)
+      {
+            printf("four");
+      }
+      else if(b==5)
+      {
+            printf("five");
+      }
+      else if(b==6)
+      {
+            printf("six");
+      }
+      else if(b==7)
+      {
+            printf("seven");
+      }
+      else if(b==8)
+      {
+            printf("eight");
+      }
+      else if(b==9)
+      {
+            printf("nine");
+      }
+      else if(b==0)
+      {
+            printf("zero");
+      }
+      
+      
+}
